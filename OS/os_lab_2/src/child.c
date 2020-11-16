8ada696c8950669c3225a671e34b0a13bc5f14b7
Maria <70853167+marianelia@users.noreply.github.com>
2020-11-16 22:06:47 +0300

    Update child.c

--- OS/os_lab_2/src/child.c
+++ OS/os_lab_2/src/child.c
@@ -12,7 +12,9 @@ int main(int argc, const char* argv[]) {
 	int count_digit = 0;
 	int number = 0;
 	int result = 0;
-        int f_ch = 1;
+	int f_not_left_minus = 0;
+	int f_ch = 1;
+
 	memset(buf, '\0', MAX_DIGIT_IN_STIRING);
 
 	while (true) {
@@ -21,19 +23,27 @@ int main(int argc, const char* argv[]) {
 			return 0;
 		}
 		if (isdigit(c) || c == '-') {
+			if (c == '-' && count_digit  > 0){	//обработка ситуаций типа "1-1"
+				f_not_left_minus = 1;
+			}
 			buf[count_digit] = c;
 			count_digit++;
 		}
 		if (c == ' ' || c == '\n') {
-			if (count_digit > 0 || count_digit >= MAX_DIGIT_IN_STIRING) {
-				number = atoi(buf);
-				result += number;
+			if (f_not_left_minus == 1){
 				memset(buf, '\0', count_digit);
 				count_digit = 0;
-			}
+				f_not_left_minus = 0;
+			}else
+				if (count_digit > 0 || count_digit >= MAX_DIGIT_IN_STIRING) {
+					number = atoi(buf);
+					result += number;
+					memset(buf, '\0', count_digit);
+					count_digit = 0;
+				}
 		}
 		if(c == '\n') {
-                        write(STDOUT_FILENO, &result, sizeof(result));
+            write(STDOUT_FILENO, &result, sizeof(result));
 			result = 0;
 		}
 	}
