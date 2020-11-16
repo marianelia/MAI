b720563f8fb5df52c4ab918004be76540cc0c95b
Maria <70853167+marianelia@users.noreply.github.com>
2020-11-16 13:33:19 +0300

    Create child.c

---
+++ OS/os_lab_2/src/child.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_DIGIT_IN_STIRING	100
+
+int main(int argc, const char* argv[]) {
+	char c;
+	char buf[MAX_DIGIT_IN_STIRING];
+	int count_digit = 0;
+	int number = 0;
+	int result = 0;
+  int f_ch = 1;
+	memset(buf, '\0', MAX_DIGIT_IN_STIRING);
+
+	while (true) {
+		f_ch = read(STDIN_FILENO, &c, sizeof(c));
+		if (f_ch == 0){
+			return 0;
+		}
+		if (isdigit(c) || c == '-') {
+			buf[count_digit] = c;
+			count_digit++;
+		}
+		if (c == ' ' || c == '\n') {
+			if (count_digit > 0 || count_digit >= MAX_DIGIT_IN_STIRING) {
+				number = atoi(buf);
+				result += number;
+				memset(buf, '\0', count_digit);
+				count_digit = 0;
+			}
+		}
+		if(c == '\n') {
+      write(STDOUT_FILENO, &result, sizeof(result));
+			result = 0;
+		}
+	}
+}
