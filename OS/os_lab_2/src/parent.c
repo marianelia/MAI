2e100257ecce4b677b169f6bd7a603cb45326627
Maria <70853167+marianelia@users.noreply.github.com>
2020-11-16 22:20:44 +0300

    Update parent.c

--- OS/os_lab_2/src/parent.c
+++ OS/os_lab_2/src/parent.c
@@ -17,13 +17,13 @@ int main(int argc, const char* argv[]){
 		read(STDIN_FILENO, &file_name, sizeof(file_name));
 		int i;
 		for (i = 0; i < 50; i++) {
-		    if (file_name[i] == '\n' || file_name[i] == '\0') {
+		    	if (file_name[i] == '\n' || file_name[i] == '\0') {
 		  		break;
-			  }
+			}
 		}
-	    if(file_name[i] == '\n') {
-            file_name[i] = '\0';
-        }
+	   	if(file_name[i] == '\n') {
+            		file_name[i] = '\0';
+        	}
 	} else {
 		strcpy(file_name, argv[1]);
 	}
@@ -37,24 +37,24 @@ int main(int argc, const char* argv[]){
 	pid_t pid;
   
 	switch(pid=fork()) {
- 	case -1 : //ошибка при вызове fork() 
+ 		case -1 : //ошибка при вызове fork() 
 		;char message[] = "fork error\n";
-        write(STDERR_FILENO, &message, sizeof(message) - 1);
-        return 2;
+        	write(STDERR_FILENO, &message, sizeof(message) - 1);
+        	return 2;
 
  	case 0 : //это код потомка
-        file = open(file_name, O_RDONLY);
-        if (file < 0) {
-            char message[] = "can't open file\n";
-            write(STDERR_FILENO, &message, sizeof(message) - 1);
-            return 3;
-        }
-	    dup2(file, STDIN_FILENO);
-        dup2(fd[1], STDOUT_FILENO);
-        dup2(fd[1], STDERR_FILENO);
-        close(file);
-        close(fd[0]);
-        close(fd[1]);
+        	file = open(file_name, O_RDONLY);
+        	if (file < 0) {
+            		char message[] = "can't open file\n";
+            		write(STDERR_FILENO, &message, sizeof(message) - 1);
+            		return 3;
+        	}
+		dup2(file, STDIN_FILENO);
+        	dup2(fd[1], STDOUT_FILENO);
+        	dup2(fd[1], STDERR_FILENO);
+        	close(file);
+        	close(fd[0]);
+        	close(fd[1]);
 
     if (execl("child", "child", (char *) NULL) == -1) {
          char message[] = "exec error\n";
@@ -63,15 +63,15 @@ int main(int argc, const char* argv[]){
 	}
  
  	default : //код родительского процесса
-	    close(fd[1]);
-        waitpid(pid, (int *)NULL, 0);
-        int result;
-        while (read(fd[0], &result, sizeof(result))) {
-            char buff[50];
-            sprintf(buff, "%d\n", result);
-            write(STDOUT_FILENO, &buff, strlen(buff));
+		close(fd[1]);
+        	waitpid(pid, (int *)NULL, 0);
+        	int result;
+        	while (read(fd[0], &result, sizeof(result))) {
+            	char buff[50];
+            	sprintf(buff, "%d\n", result);
+            	write(STDOUT_FILENO, &buff, strlen(buff));
         }
-      close(fd[0]);
+      	close(fd[0]);
   }
     return 0;
 }
